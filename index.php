1cd833179dc5031a493c8075b4dc549d6762ccf7
mibrahim95 <mojahedibrahim95@gmail.com>
2019-08-31 19:03:36 -0400

    testing semantic

--- index.php
+++ index.php
@@ -1,1 +1,7 @@
-<h1> Hello World</h1>
+<h2 class="ui icon header">
+  <i class="settings icon"></i>
+  <div class="content">
+    Mojahed's First Site
+    <div class="sub header">This is to test if it works</div>
+  </div>
+</h2>
