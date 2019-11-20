f16b90b238dbec8ee65371506f6cb6dce629c374
mibrahim95 <mojahedibrahim95@gmail.com>
2019-11-19 22:13:23 -0500

    trying database

--- common/DbConnect.php
+++ common/DbConnect.php
@@ -1,9 +1,9 @@
 <?php
 Class DbConnect{
     function getdbconnect(){
-        $servername = "localhost";
-        $username = "root";
-        $password = "root";
+        $servername = "bqmayq5x95g1sgr9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+        $username = "a07zwrfbrck7q903";
+        $password = "a6bfe3tehmmgm6xr";
         $mysql = new PDO("mysql:host=$servername;dbname=first_db", $username, $password);
         // set the PDO error mode to exception
         $mysql->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
