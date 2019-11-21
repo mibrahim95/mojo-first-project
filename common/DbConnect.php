0065597bc93b6b7ea5027af37174b1781e9337f7
Mojahed Ibrahim <mojahedibrahim@Mojaheds-MBP.fios-router.home>
2019-11-20 23:08:51 -0500

    test db3

--- common/DbConnect.php
+++ common/DbConnect.php
@@ -4,6 +4,7 @@ Class DbConnect{
         $servername = "";
         $username = "";
         $password = "";
+        $database = "";
         $whitelist = array(
             '127.0.0.1',
             '::1'
@@ -13,13 +14,15 @@ Class DbConnect{
             $servername = "localhost";
         $username = "root";
         $password = "root";
+        $database = "first_db";
         }else{
          $servername = "bqmayq5x95g1sgr9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
          $username = "a07zwrfbrck7q903";
          $password = "a6bfe3tehmmgm6xr";
+         $database = "v49paqksf2uuhzyd";
         }
         
-        $mysql = new PDO("mysql:host=$servername;dbname=v49paqksf2uuhzyd", $username, $password);
+        $mysql = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
         // set the PDO error mode to exception
         $mysql->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $mysql;
